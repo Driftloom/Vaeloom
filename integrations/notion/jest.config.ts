8fb869856b11aae568e03e1736cd458169dfbeca
Bappadala Rohith Kumar Naidu <198095685+rohitkumarnaidu@users.noreply.github.com>
2026-07-18 20:28:03 +0530

    chore(plugins): add build configs for integrations and services

---
+++ integrations/notion/jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.test\\.ts$',
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
+};
+
+export default config;
